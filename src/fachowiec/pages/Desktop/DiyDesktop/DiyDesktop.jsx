a1ea97be2e8be0a2444c5e538a583db1dee188a6
przemokoduje <145247252+przemekoduje@users.noreply.github.com>
2025-07-14 14:04:11 +0200

    opis

--- src/fachowiec/pages/Desktop/DiyDesktop/DiyDesktop.jsx
+++ src/fachowiec/pages/Desktop/DiyDesktop/DiyDesktop.jsx
@@ -6,31 +6,31 @@ import { collection, onSnapshot } from "firebase/firestore";
 const categoryColors = {
   hydraulika: {
     base: [
-      "#E5F1FB", "#D5E7F6", "#BFD8F2", "#A7C6E8", "#99BDE4", "#85ACD1", "#7397BD", "#648BB8"
+      "#E5F1FB", "#D5E7F6", "#BFD8F2", "#A7C6E8", "#99BDE4", "#85ACD1", "#7397BD"
     ],
     active: "#648BB8"
   },
   elektryka: {
     base: [
-      "#FFF0E6", "#FFE0CC", "#FFD3C2", "#FFC4A8", "#FFB294", "#F79F7A", "#E98C63", "#D9784C"
+      "#FFF0E6", "#FFE0CC", "#FFD3C2", "#FFC4A8", "#FFB294", "#F79F7A", "#E98C63"
     ],
     active: "#D9784C"
   },
   regulacja: {
     base: [
-      "#E7F6F1", "#D3EFE6", "#BFE9DC", "#A8E0CD", "#9FC8B9", "#87B29D", "#6D9C87", "#5A8974"
+      "#E7F6F1", "#D3EFE6", "#BFE9DC", "#A8E0CD", "#9FC8B9", "#87B29D", "#6D9C87"
     ],
     active: "#5A8974"
   },
   montaz: { // UWAGA! bez ogonka!
     base: [
-      "#FFFFFB", "#FDFBE8", "#FAF7D2", "#F8F3C9", "#F2EA9F", "#EDE67A", "#E5DC48", "#D8CE29"
+      "#FFFFFB", "#FDFBE8", "#FAF7D2", "#F8F3C9", "#F2EA9F", "#EDE67A", "#E5DC48"
     ],
     active: "#D8CE29"
   },
   "c.o.": {
     base: [
-      "#F6F5FD", "#E9E3FA", "#E0DAF8", "#D9D2F6", "#C2B6EB", "#AB9CDE", "#9581D1", "#8264C6"
+      "#F6F5FD", "#E9E3FA", "#E0DAF8", "#D9D2F6", "#C2B6EB", "#AB9CDE", "#9581D1"
     ],
     active: "#8264C6"
   },
